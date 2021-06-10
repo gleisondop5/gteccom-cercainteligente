0ba3b879f078621b26f56567cf93d76d04722d60
gleisondop5 <gleisonlima@id.uff.br>
2021-06-10 15:24:30 -0300

    Add da função delete

--- server/monitor/views.py
+++ server/monitor/views.py
@@ -1,11 +1,12 @@
 import cv2, json, numpy, os, threading, time, logging
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from channels.layers import get_channel_layer
 from django.conf import settings
 from django.http import HttpResponse, JsonResponse, StreamingHttpResponse, HttpResponseServerError
 from django.template import loader
 from django.views.decorators import gzip
 from queue import Queue, Full, Empty
+from django.contrib import messages
 from .models import Layer, ControlPoint, Camera, DetectedLicensePlate
 from .forms import LayerForm, ControlpointForm, CameraForm
 
@@ -40,9 +41,7 @@ def placaListView(request):
                             'FROM monitor_layer as layer, monitor_controlpoint as controlpoint, monitor_camera as camera, monitor_detectedlicenseplate as placa ' 
                             'WHERE layer.id = controlpoint.layer_id and controlpoint.id = camera.controlpoint_id and camera.id = placa.camera_id')
     
-    context = {
-        'placa_list': placa_list,
-    }
+    context = {'placa_list': placa_list}
     return render(request, "monitor/placaList.html", context)
 
 
@@ -61,6 +60,8 @@ def add_layer(request):
             layer = form.save(commit=False)
             layer.save()
             return redirect('/administracao')
+        else:
+            return redirect('/erro')
     else:    
         form = LayerForm()
         return render(request, "monitor/add_layer.html", {'form': form})
@@ -73,6 +74,8 @@ def add_controlpoint(request):
             controlpoint = form.save(commit=False)
             controlpoint.save()
             return redirect('/administracao')
+        else:
+            return redirect('/erro')
     else:    
         form = ControlpointForm()
         return render(request, "monitor/add_controlpoint.html", {'form': form})
@@ -84,9 +87,110 @@ def add_camera(request):
             camera = form.save(commit=False)
             camera.save()
             return redirect('/administracao')
+        else:
+            return redirect('/erro')
     else:    
         form = CameraForm()
         return render(request, "monitor/add_camera.html", {'form': form})
+
+def update_layer(request, id):
+    layer = get_object_or_404(Layer, pk=id)
+    form = LayerForm(instance=layer)
+    if request.method == "POST":
+        form = LayerForm(request.POST, instance=layer)
+        if form.is_valid():
+            layer.save()
+            return redirect('/administracao') 
+        else:
+            return redirect('/erro')
+    else:    
+        return render(request, "monitor/update_layer.html", {'form': form, 'layer': layer})
+
+def update_controlpoint(request, id):
+    controlpoint = get_object_or_404(ControlPoint, pk=id)
+    form = ControlpointForm(instance=controlpoint)
+    if request.method == "POST":
+        form = ControlpointForm(request.POST, instance=controlpoint)
+        if form.is_valid():
+            controlpoint.save()
+            return redirect('/administracao') 
+        else:
+            return redirect('/erro')
+    else:    
+        return render(request, "monitor/update_controlpoint.html", {'form': form, 'layer': controlpoint})
+
+def update_camera(request, id):
+    camera = get_object_or_404(Camera, pk=id)
+    form = CameraForm(instance=camera)
+    if request.method == "POST":
+        form = CameraForm(request.POST, instance=camera)
+        if form.is_valid():
+            camera.save()
+            return redirect('/administracao') 
+        else:
+            return redirect('/erro')
+    else:    
+        return render(request, "monitor/update_camera.html", {'form': form, 'layer': camera})
+
+def delete_layer(request, id):
+    layer = get_object_or_404(Layer, pk=id)
+    if request.method == "POST":
+        layer.delete()
+        messages.info(request, 'Layer excluído com sucesso')
+        return redirect('/administracao')
+    else:
+        '''
+        layer_list = Layer.objects.filter(pk=id)
+        lista = DetectedLicensePlate.objects.filter(camera__controlpoint__layer__in=layer_list)
+        logger.info(f'=====> {lista}')
+        '''
+        controlpoint_list = ControlPoint.objects.filter(layer=layer)
+        camera_list = Camera.objects.filter(controlpoint__in=controlpoint_list)
+        placa_list = DetectedLicensePlate.objects.filter(camera__in=camera_list)
+
+        context = {
+            'layer': layer,
+            'controlpoint_list': controlpoint_list,
+            'camera_list': camera_list,
+            'placa_list': placa_list
+        }
+        return render(request, "monitor/delete_layer.html", context)
+
+    
+def delete_controlpoint(request, id):
+    controlpoint = get_object_or_404(ControlPoint, pk=id)
+    if request.method == "POST":
+        controlpoint.delete()
+        messages.info(request, 'Ponto de controle excluído com sucesso')
+        return redirect('/administracao')
+    else:
+        camera_list = Camera.objects.filter(controlpoint=controlpoint)
+        placa_list = DetectedLicensePlate.objects.filter(camera__in=camera_list)
+
+        context = {
+            'controlpoint': controlpoint,
+            'camera_list': camera_list,
+            'placa_list': placa_list
+        }
+        return render(request, "monitor/delete_controlpoint.html", context)
+
+def delete_camera(request, id):
+    camera = get_object_or_404(Camera, pk=id)
+    if request.method == "POST":
+        camera.delete()
+        messages.info(request, 'Câmera excluído com sucesso')
+        return redirect('/administracao')
+    else:
+        placa_list = DetectedLicensePlate.objects.filter(camera=camera)
+
+        context = {
+            'camera': camera,
+            'placa_list': placa_list
+        }
+        return render(request, "monitor/delete_camera.html", context)
+
+def erro(request):
+    return render(request, "monitor/erro.html")
 
 
 def rtsp_panel(request, controlpoint_id, monitor_id):
